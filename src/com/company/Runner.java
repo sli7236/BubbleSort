154512fa569d466b6d300b2def306e84a11ecbc3
BT_1N3_27 <sil7236@bths.edu>
2018-11-14 11:06:24 -0500

    Initial commit

---
+++ src/com/company/Runner.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        int[] runnerArray = {8, 6, 7, 5, 3, 0};
+        bubbleSwap(runnerArray);
+        for (int i = 0; i < runnerArray.length; i++)
+        {
+            System.out.print(runnerArray[i]);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void bubbleSwap(int[] arr)
+    {
+        for (int i = 0; i < arr.length; i++)
+        {
+            for (int j = 0; j < arr.length -1; j++)
+            {
+                if (arr[j] > arr[j+1])
+                {
+                    swap(arr, j, j+1);
+                }
+            }
+        }
+    }
+}
